0e4deb935a43e0df9677d87cac1e86b320595d5d
Hamzah34 <104688862+Hamzah34@users.noreply.github.com>
2022-05-18 17:38:39 +0500

    File Uploads

---
+++ src/main/java/com/homechrome/registeration/RegisterationServlet.java
@@ -0,0 +1,64 @@
+package com.homechrome.registeration;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterationServlet
+ */
+@WebServlet("/register")
+public class RegisterationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname = request.getParameter("name");
+		String uemail = request.getParameter("email");
+		String upassword = request.getParameter("pass");
+		String umobile = request.getParameter("contact");
+		RequestDispatcher dispatcher = null;
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/homechrome?useSSL=false", "root", "root");
+			PreparedStatement pst = con.prepareStatement("insert into users(name, email, password, mobile) values(?, ?, ?, ?) ");
+			pst.setString(1, uname);
+			pst.setString(2, uemail);
+			pst.setString(3, upassword);
+			pst.setString(4, umobile);
+			
+			int rowCount = pst.executeUpdate();
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			if (rowCount > 0) {
+				request.setAttribute("status", "success");
+			}else {
+				request.setAttribute("status", "failed");
+			}
+			
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+
+}
+}
